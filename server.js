97f95ca430dea67b0005dd0dd845197180d25b1e
wittymanalipanchal-blip <witty.manalipanchal@gmail.com>
2026-03-12 17:55:15 +0530

    commit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/project-upload", require("./routes/projectUploadRoutes"));
 
 mongoose
-  .connect("mongodb+srv://dbuser:dbuser123@cluster0.mongodb.net/task_manager")
+  .connect("mongodb+srv://dbuser:dbuser123@user.pe23kpw.mongodb.net/?appName=User")
   .then(async () => {
     console.log("MongoDB Connected");
     await seedRoles();
