bdfcf2c139beb126b7b70289ccff564c5374410e
wittymanalipanchal-blip <witty.manalipanchal@gmail.com>
2026-03-16 12:35:28 +0530

    change

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 200 * 1024 * 1024 },
+  limits: { fileSize: 10000 * 1024 * 1024 },
 });
 
 router.get("/", async (req, res) => {
