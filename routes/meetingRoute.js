5a1da9ea25d2cc5936e52258a026432e7ce8e0d1
wittymanalipanchal-blip <witty.manalipanchal@gmail.com>
2026-03-16 15:36:26 +0530

    change

--- routes/meetingRoute.js
+++ routes/meetingRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Meeting = require("../models/Meeting");
+const Meeting = require("../models/meeting");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
