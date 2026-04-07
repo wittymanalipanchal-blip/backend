271cc5fe12de801d7f20ecc1ac960a2661f92ba7
wittymanalipanchal-blip <witty.manalipanchal@gmail.com>
2026-04-07 12:14:58 +0530

    change

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -8,7 +8,23 @@ router.post("/send", chatController.sendMessage);
 router.get("/all", chatController.getChat);
 router.get("/groups/:userName", chatController.getGroupChats);
 router.delete('/delete/:chatId', chatController.deleteChat);
+router.delete("/chat/:id", async (req, res) => {
+  try {
+    const chatId = req.params.id;
 
+    const deletedChat = await Chat.findByIdAndDelete(chatId);
+
+    if (!deletedChat) {
+      return res.status(404).json({ message: "Chat not found" });
+    }
+
+    res.json({ message: "Chat deleted successfully" });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 router.get("/:chatId", chatController.getChatById);
 
