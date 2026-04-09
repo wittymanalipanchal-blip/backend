7fb0dfb2478c1328dacc6bc0ad4468a2d49a2313
wittymanalipanchal-blip <witty.manalipanchal@gmail.com>
2026-04-09 15:27:36 +0530

    change

--- models/ProjectUpload.js
+++ models/ProjectUpload.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const projectUploadSchema = new mongoose.Schema({
   project: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true },
   uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   description: { type: String },
   type: { type: String, enum: ["zip", "txt", "link"], default: "zip" },
-    files: [
+  files: [
     {
       fileName: String,
-      fileBase64: String
+      fileBase64: String,
+      fileBuffer: Buffer,
+      fileType: String
     }
   ],
   status: { type: String, enum: ["Pending", "Approved", "Rejected"], default: "Pending" },
